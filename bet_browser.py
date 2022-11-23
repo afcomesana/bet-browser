09d567362294097be39049dad068ea9e48ec9040
Jorge Tarancon <si.jorgetarancon@gmail.com>
2022-11-23 10:04:32 +0100

    Minor changes in bet_browser.py:     - Rename bf as bh for better understanding.     - Add documentation to some functions.     - Open the browser without the need of download manually the driver.

--- bet_browser.py
+++ bet_browser.py
@@ -3,29 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.common.exceptions import TimeoutException
+from webdriver_manager.firefox import GeckoDriverManager
 from exceptions import BrowserException
 
 class BetBrowser:
     # Load variables helpful to dig into the betting houses websites:
     def __init__(self):
         # Related to betting houses
-        bh_info_file = open('betting-houses.json')
-        self.bh_info = json.load(bh_info_file)
-        bh_info_file.close()
+        with open('betting-houses.json','r') as bh_info_file:
+            self.bh_info = json.load(bh_info_file)
         
         # Related to leagues
-        fl_info_file = open('football-leagues.json')
-        self.fl_info = json.load(fl_info_file)
-        fl_info_file.close()
+        with open('football-leagues.json','r') as fl_info_file:
+            self.fl_info = json.load(fl_info_file)
 
     # ACCESSING URLS:
-    def get_url(self, bf, league = None):
-        # bf short for "betting_houses"
-        url = 'https://%s' % self.bh_info[bf]['domain']
+    def get_url(self, bh:str, league:str= None):
+        '''Build the url for accesing to matches.'''
+
+        # bh short for "betting_houses"
+        url = 'https://%s' % self.bh_info[bh]['domain']
         
         if league != None:
-            url += self.fl_info[league][bf]
+            url += self.fl_info[league][bh]
         
         return url
     
@@ -55,10 +57,10 @@ class BetBrowser:
         return success
         
     ### FIND INFORMATIVE PARTS OF ELEMENTS:
-    def get_matches(self, bf, league):
-        url = self.get_url(bf, league)
+    def get_matches(self, bh, league):
+        url = self.get_url(bh=bh, league=league)
         
-        if not self.access_to_page(url, self.bh_info[bf]['event_class_name']):
+        if not self.access_to_page(url=url, wait_for_element_class_name=self.bh_info[bh]['event_class_name']):
             raise BrowserException('Could not open page')
         
         matches = self.browser_driver.find_elements(
@@ -71,9 +73,14 @@ class BetBrowser:
         
     ### OPEN AND CLOSE THE WEB BROWSER:
     def open_browser(self):
-        self.browser_driver = webdriver.Firefox()
+        '''Open a Firefox web browser.'''
+        try:
+            self.browser_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        except Exception as e:
+            print(f'Could not open a browser:\n{e}.')
         
     def close_browser(self):
+        '''Close opened web browser.'''
         self.browser_driver.quit()
         
     
