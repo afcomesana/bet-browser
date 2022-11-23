1bab90f65b36d20b6172ad19861f302921c3ed06
Jorge Tarancon <si.jorgetarancon@gmail.com>
2022-11-23 10:07:45 +0100

    Rename bf as bh for better understanding.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from bet_browser import BetBrowser
 
-bet_browser = BetBrowser()
+if __name__ == '__main__':
+    bet_browser = BetBrowser()
 
-try:
-    bet_browser.open_browser()
-    bet_browser.get_matches('ps', 'spain-second-league')
-finally:
-    bet_browser.close_browser()
+    try:
+        bet_browser.open_browser()
+        bet_browser.get_matches(bh='ps', league='spain-second-league')
+    finally:
+        bet_browser.close_browser()
